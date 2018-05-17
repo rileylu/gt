2ace7529f47f20345626e799bb5959ec4ea30c27
riley <rileylu@yeah.net>
2018-05-17 19:29:48 +0800

    commit

--- test/test.py
+++ test/test.py
@@ -1,20 +1,43 @@
-from zato.server.service import Service
-from zato.server.service import ZatoException
+# from zato.server.service import Service
+# from zato.server.service import ZatoException
+#
+#
+# class BaozunException(ZatoException):
+#     pass
+#
+#
+# class TestService(Service):
+#     def handle(self):
+#         raise BaozunException(msg='hehe')
+#
+#
+# class TestClientService(Service):
+#     def handle(self):
+#         try:
+#             # self.invoke('baozun-pull-once.baozun-pull-once')
+#             self.invoke('test.test-service')
+#         except ZatoException, e:
+#             self.logger.error(e.message)
+
+import time
+import random
+import threading
 
 
-class BaozunException(ZatoException):
-    pass
+def run():
+    i = 1
+    while i < 10:
+        time.sleep(random.random())
+        print threading.current_thread().name, i
+        i += 1
 
 
-class TestService(Service):
-    def handle(self):
-        raise BaozunException(msg='hehe')
+if __name__ == '__main__':
+    from concurrent import futures
 
-
-class TestClientService(Service):
-    def handle(self):
-        try:
-            # self.invoke('baozun-pull-once.baozun-pull-once')
-            self.invoke('test.test-service')
-        except ZatoException, e:
-            self.logger.error(e.message)
+    with futures.ThreadPoolExecutor(max_workers=None) as exe:
+        fds = []
+        fds.append(exe.submit(fn=run))
+        fds.append(exe.submit(fn=run))
+        fds.append(exe.submit(fn=run))
+    print "=================================================================================="
